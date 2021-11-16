700a9f0c3afb61bdda4e59655b9477d54d82d89b
unknown <kingsleytwum2@gmail.com>
2021-11-16 13:48:16 +0000

    fixed another conflict

--- db/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectDB = (url)=>{
-    return mongoose.connect(url,{
-        useNewUrlParser: true,
-    })
-}
-
-module.exports= connectDB
--- routes/departments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const departmentRouter = express.Router();
-const { getAllDepartment,getSingleDepartment,createDepartment,updateDepartment,deleteDepartment}= require('../controllers/departments')
-
-
-departmentRouter.route('/').get(getAllDepartment).post(createDepartment)
-departmentRouter.route('/:id').get(getSingleDepartment).patch(updateDepartment).delete(deleteDepartment)
-
-module.exports=departmentRouter;
